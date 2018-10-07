096e14f91edc4ee4c83bdf586d311bfd650a8085
idawazoo <idawazoo@gmail.com>
2018-10-07 09:15:40 -0400

    work on book - post rest

---
+++ app5.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, make_response
+from flask import abort
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = {
+        'username': request.json['username'],
+        'email': request.json['email'],
+        'name': request.json.get('name',""),
+        'password': request.json['password']
+    }
+    return jsonify({'status': add_user(user)}), 201
+
+@app.errorhandler(409)
+def user_found(error):
+    return make_response(jsonify({'error': 'Conflict! Record exist'}), 409)
+
+@app.errorhandler(400)
+def invalid_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+def add_user(new_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    a_dict=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? or email=?",(new_user['username'],new_user['email']))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        abort(409)
+    else:
+       cursor.execute("insert into users (username, email, password, full_name) values(?,?,?,?)",(new_user['username'],new_user['email'], new_user['password'], new_user['name']))
+       conn.commit()
+       return "Success"
+    conn.close()
+    return jsonify(a_dict)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ virtualenv-setup.sh
@@ -0,0 +1,2 @@
+virtualenv env --no-site-packages --python=python3
+source env/bin/activate
