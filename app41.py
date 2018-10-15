f5b9f388db3f670ea0e2d0462def40bcc89132b9
idawazoo <idawazoo@gmail.com>
2018-10-14 22:15:13 -0400

    update mongodb for users

--- app41.py
+++ app41.py
@@ -10,6 +10,7 @@ from flask import redirect, session
 from flask import current_app
 import flask
 from pymongo import MongoClient
+import random
 
 app = Flask(__name__)
 app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
@@ -97,6 +98,7 @@ def list_tweet(user_id):
     return jsonify(user)
 
 def add_user(new_user):
+    """
     conn = sqlite3.connect('mydb.db')
     print ("Opened database successfully");
     api_list=[]
@@ -111,9 +113,31 @@ def add_user(new_user):
        return "Success"
     conn.close()
     return jsonify(a_dict)
+    """
+    api_list = []
+    print(new_user)
+    db = connection.cloud_native.users
+    user = db.find({'$or': [
+        {
+            "username": new_user['username']
+        },
+        {
+            "email": new_user['email']
+        }
+    ]
+    })
+    for i in user:
+        print (str(i))
+        api_list.append(str(i))
 
+    if api_list == []:
+        db.insert(new_user)
+        return "success"
+    else:
+        abort(409)
 
 def del_user(del_user):
+    """
     conn = sqlite3.connect('mydb.db')
     print ("Opened database successfully");
     cursor=conn.cursor()
@@ -126,6 +150,18 @@ def del_user(del_user):
        cursor.execute("delete from users where username==?",(del_user,))
        conn.commit()
        return "Success"
+    """
+
+    db = connection.cloud_native.users
+    api_list = []
+    for i in db.find({ 'username': del_user}):
+        api_list.append(str(i))
+
+    if api_list == []:
+        abort(404)
+    else:
+        db.remove({"username": del_user})
+        return "success"
 
 def list_tweets():
     conn = sqlite3.connect('mydb.db')
@@ -169,6 +205,7 @@ def add_tweet(new_tweets):
        return "Success"
 
 def upd_user(user):
+    '''
     conn = sqlite3.connect('mydb.db')
     print ("Opened database successfully");
     cursor=conn.cursor()
@@ -186,7 +223,21 @@ def upd_user(user):
                 cursor.execute("""UPDATE users SET {0} = ? WHERE id = ?""".format(i), (user[i], user['id']))
                 conn.commit()
     return "Success"
+    '''
 
+    api_list=[]
+    print(user)
+
+    db_user = connection.cloud_native.users
+    users = db_user.find_one({"id": user['id']})
+    for i in users:
+        api_list.append(str(i))
+
+        if api_list == []:
+            abort(409)
+        else:
+            db_user.update({'id':user['id']}, {'$set': user}, upsert=False)
+            return "Success"
 
 
 @app.route("/api/v1/info")
@@ -232,6 +283,8 @@ def create_user():
         'name': request.json.get('name',""),
         'password': request.json['password']
     }
+    user['id'] = random.randint(1,1000)
+
     return jsonify({'status': add_user(user)}), 201
 
 @app.route('/api/v1/users', methods=['DELETE'])
